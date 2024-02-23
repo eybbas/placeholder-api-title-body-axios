7c21669053410034087df50b6ffa602a1b0cdc55
Abbos <eybbas@gmail.com>
2024-02-23 10:11:00 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//2. Реализуйте функциональный компонент с использованием хука useState, который будет отображать текстовое поле и кнопку. 
+//При вводе значения в текстовое поле и нажатии на кнопку, значение должно сохраняться в состоянии и отображаться под текстовым полем.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App(props){
+  const textRef = React.createRef()
+
+  let [text, setText] = useState(null)
+
+  const showText = () => {
+    setText(textRef.current.value)
+  }
+
+  const usersdiv = <div><p>{text}</p></div>
+
+  return(
+    <>
+    <input type='text' ref={textRef} placeholder='enter text'/>
+    <button onClick={showText}>Show Text</button>
+    {usersdiv}
+    </>
+  )
+
+}
+
+const root = document.getElementById("root");
+ReactDOM.render(<App />, root);
