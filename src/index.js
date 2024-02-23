e722cd3048f26f2bbd8da01019641c01e8e2094d
Abbos <eybbas@gmail.com>
2024-02-23 10:48:21 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-//2. Реализуйте функциональный компонент с использованием хука useState, который будет отображать текстовое поле и кнопку. 
-//При вводе значения в текстовое поле и нажатии на кнопку, значение должно сохраняться в состоянии и отображаться под текстовым полем.
+//1. Создайте функциональный компонент с использованием хука useState, который отобразит кнопку и счетчик. 
+//При каждом нажатии на кнопку счетчик должен увеличиваться на 1.
 
 function App(props){
-  const textRef = React.createRef()
+  let [number, setNum] = useState(0)
 
-  let [text, setText] = useState(null)
-
-  const showText = () => {
-    setText(textRef.current.value)
+  const numberPlus = (number) => {
+    number++
+    setNum(number);
   }
 
-  const usersdiv = <div><p>{text}</p></div>
+  const usersdiv = <div><p>{number}</p></div>
 
   return(
     <>
-    <input type='text' ref={textRef} placeholder='enter text'/>
-    <button onClick={showText}>Show Text</button>
+    <button onClick={() => numberPlus(number)}>Plus</button>
     {usersdiv}
     </>
   )
