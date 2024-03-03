e2990c7637ceba4128bd30b4e4a760967e835a46
Abbos <eybbas@gmail.com>
2024-03-03 20:27:10 +0500

    example

--- src/index.js
+++ src/index.js
@@ -1,24 +1,59 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css"
 
-//1. Создайте функциональный компонент с использованием хука useState, который отобразит кнопку и счетчик. 
-//При каждом нажатии на кнопку счетчик должен увеличиваться на 1.
+//3. Создайте функцию-редуктор с использованием хука useState, которая будет управлять состоянием массива задач. 
+//Функция-редуктор должна поддерживать добавление новой задачи, удаление задачи по индексу и изменение статуса выполнения задачи.
 
 function App(props){
-  let [number, setNum] = useState(0)
+  const inputRef = React.createRef()
+  const inputTaskRef = React.createRef()
+  const inputCheckRef = React.createRef()
+  let [toDos, setToDos] = useState([])
+  let [toDoStatus, setToDoStatus] = useState(false)
+  //const [selectedValue, setSelectedValue] = useState('');
 
-  const numberPlus = (number) => {
-    number++
-    setNum(number);
+  //const text_decoration = { textDecoration: "line-through" }
+
+  const createTask = () => {
+    let toDoName = inputRef.current.value
+    setToDos([...toDos, {id: toDos.length + 1, title: toDoName, status: toDoStatus}])
+  }
+  const deleteToDos = (deleteToDo) => {
+    let newToDos = toDos.filter((toDo) => toDo !== deleteToDo)
+    setToDos(newToDos);
+    //console.log(newTodos);
   }
 
-  const usersdiv = <div><p>{number}</p></div>
+  const toDoStatusFunc = () => {
+    setToDoStatus(!toDoStatus)
+  }
 
+  function TasksDiv({toDos}){
+    //console.log(toDos);
+    return (
+      <>
+        {toDos.map((task) => (
+          <div className='task-div' id={task.id}>
+            <input type='checkbox' ref={inputCheckRef} key={"checkbox" + task.id} id={task.id} onChange={toDoStatusFunc}/>
+            {console.log(inputCheckRef) }
+            <span className={'span' + task.id} ref={inputTaskRef} key={"span" + task.id}>{task.title}</span>
+            <button key={"remove" + task.id} onClick ={() => deleteToDos(task)}>Remove</button>
+          </div>
+        ))}
+      </>
+    )
+  }
   return(
-    <>
-    <button onClick={() => numberPlus(number)}>Plus</button>
-    {usersdiv}
-    </>
+    <div className='main-div'>
+      <div className='input-div'>
+        <input className='task-text' type='text' ref={inputRef}/>
+        <button className='add-task-btn' onClick={createTask}>Plus</button>
+      </div>
+      <div className='todo-div'>
+        <TasksDiv toDos={toDos}/>
+      </div>
+    </div>
   )
 
 }
