5b7fe7c353c53359a52bc74c69bb7fd2b9b65755
Abbos <eybbas@gmail.com>
2024-03-11 19:12:35 +0500

    Context

--- src/index.js
+++ src/index.js
@@ -1,34 +1,51 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
+import userPic from './img/userPic.png'
 
 const ThemeContext = createContext();
 
-function App(){
-
+function ThemeToggle(){
   const [theme, setTheme] = useState('light');
-  const [btnColor, setBtnColor] = useState('black');
 
-  const themeToogle = () =>{
-    if(theme === 'light'){
-      setTheme('dark');
-      setBtnColor('white')
-      document.body.style.background = 'black';
-    }else{
-      setTheme('light');
-      setBtnColor('black')
-      document.body.style.background = 'white'
-    }
+  const themeToggle = () => {
+    setTheme((prev) => (prev === 'light'? 'dark' : 'light'));
   }
-
   return(
-    <ThemeContext.Provider value={theme}>
-        <div className='btn-div'>
-            <button style={{color: btnColor}} onClick={themeToogle}>{theme === 'light'? 'Light.' : 'Dark.'}</button>
-        </div>
-    </ThemeContext.Provider>
+  <ThemeContext.Provider value={theme}>
+    <div className='theme-radio-div' style={{backgroundColor: theme === "light" ? "#f4f4f4" : "#1E1E1E" }}>
+      <span className='theme-title-span' style={{ color: theme === "dark" ? "white" : "black" }}>Dark Mode: </span>
+      <label className='theme-label' htmlFor='themeToggle'>
+        <input id='themeToggle' className='theme-radio' type='checkbox' name='themeToggle' onChange={themeToggle}/>
+        <span className='theme-slider' />
+      </label>
+    </div>
+    <App />
+  </ThemeContext.Provider>
+    
+  )
+}
+function App(){
+  const theme = useContext(ThemeContext);
+  if(theme === 'light'){
+    document.body.style.background = '#d6d6d6';
+  }else{
+    document.body.style.background = '#292929'
+  }
+  return(
+    <div className='user-card-div' style={{backgroundColor: theme === "light" ? "#f4f4f4" : "#1E1E1E" }}>
+      <div className='user-pic-div'>
+        <img className='user-pic-img' src={userPic}/>
+      </div>
+      <div className='user-info-div'>
+        <span className='user-name-span' style={{ color: theme === "dark" ? "white" : "black" }}>@sxtell</span>
+        <span className='user-prof-span' style={{ color: theme === "dark" ? "white" : "black" }}>artist</span>
+        <span className='user-followers-span' style={{ color: theme === "dark" ? "white" : "black" }}>12.3M followers</span>
+        <button className='user-follow-btn'>follow</button>
+      </div>
+    </div>
   )
 }
 
 const root = document.getElementById("root");
-ReactDOM.render(<App />, root);
+ReactDOM.render(<ThemeToggle />, root);
