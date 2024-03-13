82568c9550ae20a9552b309623d89963ebd53fe7
Abbos <eybbas@gmail.com>
2024-03-13 23:15:52 +0500

    Context

--- src/index.js
+++ src/index.js
@@ -1,51 +1,93 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 import userPic from './img/userPic.png'
+import userNotLog from './img/userNotLog.png'
 
-const ThemeContext = createContext();
+const UserContext = createContext();
 
-function ThemeToggle(){
-  const [theme, setTheme] = useState('light');
+function UserLogIn(){
+  const [user, setUser] = useState([])
+  const nameRef = useRef()
+  const mailRef = useRef()
 
-  const themeToggle = () => {
-    setTheme((prev) => (prev === 'light'? 'dark' : 'light'));
+  const getUserInfo = () => {
+    let name = nameRef.current.value;
+    let mail = mailRef.current.value;
+    let userInfo = [{name: name, email: mail}];
+    setUser(userInfo);
+    
+    console.log(user.name);
   }
   return(
-  <ThemeContext.Provider value={theme}>
-    <div className='theme-radio-div' style={{backgroundColor: theme === "light" ? "#f4f4f4" : "#1E1E1E" }}>
-      <span className='theme-title-span' style={{ color: theme === "dark" ? "white" : "black" }}>Dark Mode: </span>
-      <label className='theme-label' htmlFor='themeToggle'>
-        <input id='themeToggle' className='theme-radio' type='checkbox' name='themeToggle' onChange={themeToggle}/>
-        <span className='theme-slider' />
-      </label>
-    </div>
-    <App />
-  </ThemeContext.Provider>
+    <UserContext.Provider value={user}>
+      <div className='login-div'>
+        <div className='login-title-div'>
+          <span className='login-title-span'>Log In:</span>
+        </div>
+        <div className='username-div'>
+          <label className='username-label' htmlFor='username'>Username:</label>
+          <input className='username-input' ref={nameRef} type='text' id='username' placeholder='sxtell'/>
+        </div>
+        <div className='email-div'>
+          <label className='email-label' htmlFor='email'>Email:</label>
+          <input className='email-input' ref={mailRef} type='email' id='email' placeholder='eybbas@gmail.com'/>
+        </div>
+        <div className='login-btn-div'>
+          <button className='login-btn' onClick={getUserInfo}>Log In</button>
+          <button className='signup-btn' onClick={getUserInfo}>Sign Up</button>
+        </div>
+      </div>
+      <UserProfile/>
+    </UserContext.Provider>
+  )
+}
+function UserProfile(){
+  const user = useContext(UserContext);
+  console.log(user);
+  return(
+    <>
+      {user.map((user) =>{
+        if(user.name === '' || user.email === ''){
+          return(
+          <div className='profile-div'>
+            <div className='profile-pic-div'>
+              <img className='profile-pic-img' src={userNotLog} />
+            </div>
+            <div className='profile-not-logged-div'>
+              <span className='profile-not-logged-span'>User Is Not Logged In</span>
+              <span className='profile-not-logged-link-span'>We could not find information about this user. <a className='profile-not-logged-link' src=''>Read more in detail.</a></span>
+            </div>
+          </div>
+          )
+        }
+        else{
+          return(
+          <div className='profile-div'>
+            <div className='profile-pic-div'>
+              <img className='profile-pic-img' src={userPic} />
+            </div>
+            <div className='profile-info-div'>
+              <span className='profile-username-span'>@{user.name}</span>
+              <span className='profile-subscribers-span'>12.3M followers</span>
+              <span className='profile-email-span'>email: {user.email}</span>
+              <button className='profile-moreinfo-btn'>More Information</button>
+            </div>
+          </div>
+          )
+        }
+          
+      })}
+    </>
     
   )
 }
 function App(){
-  const theme = useContext(ThemeContext);
-  if(theme === 'light'){
-    document.body.style.background = '#d6d6d6';
-  }else{
-    document.body.style.background = '#292929'
-  }
   return(
-    <div className='user-card-div' style={{backgroundColor: theme === "light" ? "#f4f4f4" : "#1E1E1E" }}>
-      <div className='user-pic-div'>
-        <img className='user-pic-img' src={userPic}/>
-      </div>
-      <div className='user-info-div'>
-        <span className='user-name-span' style={{ color: theme === "dark" ? "white" : "black" }}>@sxtell</span>
-        <span className='user-prof-span' style={{ color: theme === "dark" ? "white" : "black" }}>artist</span>
-        <span className='user-followers-span' style={{ color: theme === "dark" ? "white" : "black" }}>12.3M followers</span>
-        <button className='user-follow-btn'>follow</button>
-      </div>
+    <div className='container-div'>
+      <UserLogIn/>
     </div>
   )
 }
-
 const root = document.getElementById("root");
-ReactDOM.render(<ThemeToggle />, root);
+ReactDOM.render(<App/>, root);
